3b0b475b152b5a50ece927228b0ff91dbc92ff84
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-03-15 19:45:57 +0200

    Refactor lab1.cs with new functions and logic

--- lab1.cs
+++ lab1.cs
@@ -1,87 +1,146 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Numerics;
-using System.Diagnostics.Eventing.Reader;
-using System.Windows;
-// Tools → NuGet Package Manager → Package Manager Console
-// Install-Package MathNet.Numerics
-using MathNet.Numerics.LinearAlgebra;
-
-namespace lab2
+double getSideOfCube(double volume)
 {
-    internal class Program
+    return Math.Pow(volume, 1.0 / 3);
+}
+bool is_sum_uneven(int number)
+{
+    if (number > 100) { 
+        Console.WriteLine("Number is not 2-digit.");
+        return false;
+    }
+    int sum_of_digits = (number % 10) + ((number - number % 10)/10);
+    return sum_of_digits % 2 != 0;
+}
+
+void is_in_triangle(double x, double y)
+{
+    double abs_x = Math.Abs(x);
+    double upper_boundary = -abs_x;
+    double lower_boundary = -100;
+
+    bool on_upper = (y == upper_boundary) && (y >= lower_boundary);
+    bool on_lower = (y == lower_boundary) && (abs_x <= 100);
+
+    if (on_upper || on_lower)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Input task: ");
-            int task = int.Parse(Console.ReadLine());
-            while (task > 0)
+        Console.WriteLine("On the bounds");
+    }
+
+    else if (y < upper_boundary && y > lower_boundary)
+    {
+        Console.WriteLine("Inside of bounds");
+    }
+    else
+    {
+        Console.WriteLine("Outside of bounds");
+    }
+}
+DateTime get_date(int n) { 
+    if (n < 0) {
+        Console.WriteLine("n must be non-negative.");
+        return DateTime.Now;
+    }
+    DateTime current_date = DateTime.Now;
+    return current_date.AddDays(-n);
+}
+
+int get_square_sum(int a, int b) { 
+    return (a+b)*(a+b);
+}
+
+double get_result(double a, double b) {
+    return 5 + b / (b * b + 1) + (a - b) / (a + b);
+}
+
+while (true)
+{
+    Console.WriteLine("Input task: ");
+    int task = int.Parse(Console.ReadLine()!);
+
+    switch (task)
+    {
+        case 1:
+            Console.WriteLine("Task 1: Calculate side of cube from volume.");
+            
+            Console.WriteLine("Input volume of cube: ");
+            double volume = double.Parse(Console.ReadLine()!);
+
+            double side = getSideOfCube(volume);
+            Console.WriteLine($"Side of the cube: {Math.Round(side)}");
+            
+            break;
+        
+        case 2:
+            Console.WriteLine("Task 2: Check if sum of digits is uneven.");
+
+            Console.WriteLine("Input 2-digit number: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+            if (is_sum_uneven(number))
             {
-                switch (task) 
-                {
-                    case 1:
-                        Console.WriteLine("Input array: ");
-                        int[] numbers1 = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-                        Console.Write("Input a: ");
-                        int a = int.Parse(Console.ReadLine());
-                        Console.Write("Input b: ");
-                        int b = int.Parse(Console.ReadLine());
-                        List<int> filteredNumbers = new List<int>() { };
-                        foreach (int num in numbers1)
-                        {
-                            if (num < a || num > b)
-                            {
-                                filteredNumbers.Add(num);
-                            }
-                        }
-                        Console.Write("Filtered array size: " + filteredNumbers.Count);
-                        break;
-                    case 2:
-                        Console.WriteLine("Input array: ");
-                        int[] numbers2 = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
-                        int min = numbers2[0];
-                        foreach (int num in numbers2) 
-                        {
-                            if (num < min) 
-                            { 
-                                min = num;
-                            }
-                        }
-                        Console.Write("Array minimum:" + min);
-                        break;
-                    case 3:
-                        Console.Write("Input n: ");
-                        int n = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Input matrix(n x n): ");
-                        var matrix = Matrix<double>.Build.Dense(n, n);
-                        for (int i = 0; i < n; i++)
-                        {
-                            for (int j = 0; j < n; j++)
-                            {
-                                Console.Write($"matrix[{i},{j}]: ");
-                                matrix[i, j] = int.Parse(Console.ReadLine());
-                            }
-                        }
-                        var result = matrix;
-                        for (int i = 1; i < n; i++)
-                        {
-                            result = result * matrix;
-                        }
-                        Console.WriteLine("Result: ");
-                        for (int i = 0; i < n; i++)
-                        {
-                            for (int j = 0; j < n; j++)
-                            {
-                                Console.Write(result[i, j] + " ");
-                            }
-                            Console.WriteLine();
-                        }
-                        break;
-                }
+                Console.WriteLine("Sum of digits is uneven.");
+            } 
+            else
+            {
+                Console.WriteLine("Sum of digits is even.");
             }
-        }
+            
+            break;
+        
+        case 3:
+            Console.WriteLine("Task 3: Check if point is inside the triangle.");
+            
+            Console.WriteLine("Input x: ");
+            double x = double.Parse(Console.ReadLine()!);
+            
+            Console.WriteLine("Input y: ");
+            double y = double.Parse(Console.ReadLine()!);
+            
+            is_in_triangle(x, y);
+            
+            break;
+        
+        case 4:
+            Console.WriteLine("Task 4: Get date n days ago.");
+
+            Console.WriteLine("Input n: ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            DateTime date = get_date(n);
+            Console.WriteLine($"Date {n} days ago: {date.ToShortDateString()}");
+            
+            break;
+        
+        case 5:
+            Console.WriteLine("Task 5: Get sum of squares of two numbers.");
+
+            Console.WriteLine("Input a: ");
+            int a = int.Parse(Console.ReadLine()!);
+            
+            Console.WriteLine("Input b: ");
+            int b = int.Parse(Console.ReadLine()!);
+            
+            int square_sum = get_square_sum(a, b);
+            Console.WriteLine($"Square of sum: {square_sum}");
+            
+            break;
+        
+        case 6:
+            Console.WriteLine("Task 6: Get result of the expression.");
+            
+            Console.WriteLine("Input a: ");
+            double a_double = double.Parse(Console.ReadLine()!);
+            
+            Console.WriteLine("Input b: ");
+            double b_double = double.Parse(Console.ReadLine()!);
+            
+            double result = get_result(a_double, b_double);
+            Console.WriteLine($"Result: {result}");
+            
+            break;
+        
+        default:
+            Console.WriteLine("Invalid task number.");
+            return;
     }
 }
