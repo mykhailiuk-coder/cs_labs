c6f2628c5220153d7f439cc88624087c22ec663d
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-03-19 10:00:02 +0200

    Add unit test for GetSumOfElementsInRange method
    Added a new test method to verify the sum of elements in a specified column range of a matrix.

--- test2.cs
+++ test2.cs
@@ -1,4 +1,5 @@
 using MathNet.Numerics.LinearAlgebra;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace lab2.test
 {
     [TestClass]
@@ -31,6 +32,23 @@ namespace lab2.test
             var expected = Matrix<double>.Build.DenseOfArray(new double[,] { { 7, 10 }, { 15, 22 } });
             var actual = Lab2.getMatrixToPower(2, M, power);
             Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void GetSumOfElementsInRange_SumOfElementsInSpecifiedColumnRange()
+        {
+            int[][] matrix = new int[][]
+            {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 5, 6, 7, 8 },
+                new int[] { 9, 10, 11, 12 }
+            };
+            int n = matrix.Length;
+            int m = matrix[0].Length;
+            int k1 = 1;
+            int k2 = 2;
+            int[] expected = new int[] { 5, 13, 21 };
+            int[] actual = Lab2.GetSumOfElementsInRange(matrix, n, m, k1, k2);
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
