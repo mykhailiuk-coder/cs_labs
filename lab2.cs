9c2018eb03d3a56ca005bdc6f31e9f1d54876d1b
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-03-19 09:59:37 +0200

    Refactor Lab2 class and add GetSumOfElementsInRange method

--- lab2.cs
+++ lab2.cs
@@ -1,9 +1,10 @@
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Linq;
 
-public class Lab2 
-{ 
-    public static int getNumberOutOfRange(int[] arr, int low, int high) 
+public class Lab2
+{
+    public static int getNumberOutOfRange(int[] arr, int low, int high)
     {
         int count = 0;
         foreach (int num in arr)
@@ -15,7 +16,7 @@ public class Lab2
         }
         return count;
     }
-    public static int getMinimum(int[] arr, int size) 
+    public static int getMinimum(int[] arr, int size)
     {
         if (size == 0 || arr.Length == 0)
         {
@@ -32,13 +33,32 @@ public class Lab2
         return min;
     }
 
-    public static Matrix<double> getMatrixToPower(int n, Matrix<double> M, int power) 
+    public static Matrix<double> getMatrixToPower(int n, Matrix<double> M, int power)
     {
         for (int i = 1; i < power; i++)
         {
-            M = M*M;
+            M = M * M;
         }
         return M;
+    }
+
+    public static int[] GetSumOfElementsInRange(int[][] matrix, int n, int m, int k1, int k2)
+    {
+        if (k1 < 0 || k2 > m || k1 >= k2)
+        {
+            throw new ArgumentException($"Invalid column range: {k1} to {k2}. Matrix has {m} columns.");
+        }
+
+        int[] sums = new int[n];
+
+        for (int i = 0; i < n; i++)
+        { 
+            for (int j = k1; j <= k2; j++)
+            {
+                sums[i] += matrix[i][j];
+            }
+        }
+        return sums;
     }
 }
 
@@ -49,7 +69,7 @@ public class Program
         while (true)
         {
             Console.WriteLine("Input task: ");
-            int taskNumber = int.Parse(Console.ReadLine()!);
+            int taskNumber = int.Parse(Console.ReadLine());
             switch (taskNumber)
             {
                 case 1:
@@ -61,15 +81,15 @@ public class Program
                     break;
                 case 2:
                     Console.WriteLine("Input size of array: ");
-                    int size = int.Parse(Console.ReadLine()!);
+                    int size = int.Parse(Console.ReadLine());
                     Console.WriteLine("Input array: ");
-                    int[] arr2 = Console.ReadLine()!.Trim().Split(' ').Select(int.Parse).ToArray();
+                    int[] arr2 = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
                     float min = Lab2.getMinimum(arr2, size);
                     Console.WriteLine($"Minimum element in the array: {min}");
                     break;
                 case 3:
                     Console.Write("Input n: ");
-                    int n = int.Parse(Console.ReadLine()!);
+                    int n = int.Parse(Console.ReadLine());
                     Console.WriteLine("Input matrix(n x n): ");
                     var matrix = Matrix<double>.Build.Dense(n, n);
                     for (int i = 0; i < n; i++)
@@ -77,11 +97,11 @@ public class Program
                         for (int j = 0; j < n; j++)
                         {
                             Console.Write($"matrix[{i},{j}]: ");
-                            matrix[i, j] = int.Parse(Console.ReadLine()!);
+                            matrix[i, j] = int.Parse(Console.ReadLine());
                         }
                     }
                     Console.Write("Input power: ");
-                    int power = int.Parse(Console.ReadLine()!);
+                    int power = int.Parse(Console.ReadLine());
                     var resultMatrix = Lab2.getMatrixToPower(n, matrix, power);
                     Console.WriteLine($"Matrix to the power of {power}: ");
                     for (int i = 0; i < n; i++)
@@ -91,6 +111,33 @@ public class Program
                             Console.Write(resultMatrix[i, j] + " ");
                         }
                         Console.WriteLine();
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("Input n: ");
+                    int n2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input m: ");
+                    int m2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input k1: ");
+                    int k1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input k2: ");
+                    int k2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Input matrix(n x m): ");
+                    int[][] matrix2 = new int[n2][];
+                    for (int i = 0; i < n2; i++)
+                    {
+                        matrix2[i] = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                    }
+                    try
+                    {
+                        int[] sums = Lab2.GetSumOfElementsInRange(matrix2, n2, m2, k1, k2);
+                        Console.WriteLine("Sum of elements in each row: " + string.Join(", ", sums));
+
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        break;
                     }
                     break;
             }
