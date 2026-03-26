43e8d404c3d529f938bcdd3413ee63b6266980e5
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-03-26 07:55:01 +0200

    Refactor VectorByte class with properties and operators
    Changed codeError to a property with getter and setter. Added indexer and operator overloads for VectorByte class.

--- lab4.cs
+++ lab4.cs
@@ -10,7 +10,7 @@ namespace lab4
     {
         protected byte[] BArray;
         protected uint n { get; }
-        protected int codeError;
+        protected int codeError { get; set; }
         protected static uint num_vec = 0;
 
         public VectorByte()
@@ -61,8 +61,40 @@ namespace lab4
         {
             Console.WriteLine("Number of VectorByte instances: " + num_vec);
         }
-    }
-    internal class Program
+        public int this[int index]
+        {
+            get 
+            {
+                return 0;
+            }
+            set
+            {
+                if (index < 0 || index > this.n) 
+                {
+                    this.codeError = 1;
+                }
+            }
+        }
+        public static VectorByte operator++(VectorByte vec)
+        {
+            for (int i = 0; i < vec.n; i++)
+                vec.BArray[i]++;
+            return vec;
+        }
+        public static bool operator true(VectorByte vec)
+        {
+            bool result = true;
+            if (vec.n != 0)
+                return true;
+            for (int i = 0; i < vec.n; i++) 
+            { 
+                if (vec.BArray[i] == 0)
+                    result = false;
+            }
+            return result;
+        }
+}
+    public class Program
     {
         static void Main(string[] args)
         {
